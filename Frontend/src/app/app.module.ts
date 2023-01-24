f7cdd86e74863d9906a401303846955644f85c05
gofaone96 <70521525+gofaone96@users.noreply.github.com>
2023-01-24 15:32:55 +0200

    frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SchoolsComponent } from './components/schools/schools.component';
 import { VehiclesComponent } from './components/vehicles/vehicles.component';
+import { AddvehicleComponent } from './components/addvehicle/addvehicle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SchoolsComponent,
-    VehiclesComponent
+    VehiclesComponent,
+    AddvehicleComponent,
 
   ],
   imports: [
