1de1ee985c77f36e89ea18f303cd1d724ef8a80c
clement mlambo <115017701+clementmkhuleko@users.noreply.github.com>
2023-01-25 15:42:44 +0200

    updated register,login and landing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LandingComponent } from './components/landing/landing.component';
 
-const routes: Routes = [{ path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) }];
+
+
+
+const routes: Routes = [{ path: 'landing', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) },
+{ path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path:'Landing', component: LandingComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
