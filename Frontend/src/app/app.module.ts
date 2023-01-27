bc07301dc97e321a06c6827df81a0ac7931f5dee
SiphoMemane <115017772+SiphoMemane@users.noreply.github.com>
2023-01-27 11:18:27 +0200

    backend and front register and login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SchoolsComponent } from './components/schools/schools.component';
 import { VehiclesComponent } from './components/vehicles/vehicles.component';
-<<<<<<< HEAD
 import { NavigationComponent } from './navigation/navigation.component';
 import { NavDriverComponent } from './nav-driver/nav-driver.component';
 import { NavAdminComponent } from './nav-admin/nav-admin.component';
@@ -16,25 +15,18 @@ import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-=======
-import { AddvehicleComponent } from './components/addvehicle/addvehicle.component';
->>>>>>> f7cdd86e74863d9906a401303846955644f85c05
 
 @NgModule({
   declarations: [
     AppComponent,
     SchoolsComponent,
     VehiclesComponent,
-<<<<<<< HEAD
 
     RegisterComponent,
  
        LandingComponent,
          LoginComponent,
   
-=======
-    AddvehicleComponent,
->>>>>>> f7cdd86e74863d9906a401303846955644f85c05
 
   ],
   imports: [
