521c270b6f3c84b06e56d7d48413abf8ecf53f69
CharitySelowa <115018179+CharitySelowa@users.noreply.github.com>
2023-01-31 11:04:10 +0200

    Revert "Develop"

--- Frontend/src/app/components/schools/schools.component.ts
+++ Frontend/src/app/components/schools/schools.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ParentService } from 'src/app/services/schools/parent.service';
 
 @Component({
   selector: 'app-schools',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schools.component.scss']
 })
 export class SchoolsComponent implements OnInit {
+ data:any
+ searchSchool :any
 
-  constructor() { }
+constructor(private service:ParentService) { }
 
   ngOnInit(): void {
+    this.service.getSchool().subscribe((view)=>{
+    this.data=view
+    
+  })
   }
 
 }
