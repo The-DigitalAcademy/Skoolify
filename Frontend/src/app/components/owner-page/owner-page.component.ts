69829a8813af562a6648b514087c0e582ee1be8a
CharitySelowa <115018179+CharitySelowa@users.noreply.github.com>
2023-01-31 11:31:15 +0200

    Revert "Revert "Revert "Develop"""
    This reverts commit cf7a1b6e53efed6031c9971cd21bf871a4017766.

---
+++ Frontend/src/app/components/owner-page/owner-page.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ParentService } from 'src/app/services/schools/parent.service';
+
+@Component({
+  selector: 'app-owner-page',
+  templateUrl: './owner-page.component.html',
+  styleUrls: ['./owner-page.component.scss']
+})
+export class OwnerPageComponent implements OnInit {
+  data:any
+  driver:any
+  searchSchool :any
+
+ constructor(private service:ParentService) { }
+
+  // ngOnInit(): void {
+  //   this.service.getDrivers().subscribe((view)=>{
+  //   this.data=view
+
+  // })
+  // }
+  ngOnInit(): void {
+    this.service.getDrivers().subscribe((view)=>{
+    this.driver=view
+    console.log("selected id", view)
+  })
+  this.service.getSchool().subscribe((view)=>{
+    this.data=view
+    //console.log("selected id", view)
+  })
+
+  }
+}
---
+++ Frontend/src/app/components/register/register.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthusersService } from 'src/app/services/authusers.service';
+import { HttpClient } from '@angular/common/http';
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private auth: AuthusersService) { }
+
+  ngOnInit(): void {
+  }
+
+  registerForm1 = new FormGroup({
+
+    email: new FormControl('',[Validators.required, Validators.email]),
+    name: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]$')]),
+    surname: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]$')]),
+    password: new FormControl('', [Validators.required, Validators.min(6)]),
+    account: new FormControl(),
+    confirmPassword: new FormControl(),
+    gender :new FormControl(),
+  })
+  step=1
+  
+  nextStep(form:FormGroup)
+    {
+      this.step = 2;
+  
+    }
+    
+    onRegister(form:FormGroup){
+      if (this.registerForm1.valid) {
+        // submit the form
+        }
+      console.log(form.value)
+   //Add the User to the Database
+   this.auth.postData(form.value).subscribe((results)=>{
+     if(results == 'User added to database'){
+
+      console.warn('sucess');
+      alert('successfully registered');
+      
+     }
+     else{
+      alert(results)
+      
+     }
+     
+   })
+  }
+
+    
+
+
+
+    
+}
