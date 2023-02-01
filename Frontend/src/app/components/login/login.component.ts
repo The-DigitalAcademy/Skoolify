eda6bf7dbe094ec4ce41ce0afbf091521b268c9c
KagisoMonageng <kagisoboasmonageng@gmail.com>
2023-02-01 12:48:25 +0200

    Login and register ts and backend fix

--- Frontend/src/app/components/login/login.component.ts
+++ Frontend/src/app/components/login/login.component.ts
@@ -1,64 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators,AbstractControl, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  FormBuilder,
+} from '@angular/forms';
 import { AuthusersService } from 'src/app/services/authusers.service';
 import { Router } from '@angular/router';
+import { JwtService } from 'src/app/services/jwt.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { User } from 'src/app/interfaces/user';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-submitted: any;
+  submitted: any;
+  role: any;
 
+  user : User| null = {
+    user_id: 0,
+    name: '',
+    surname: '',
+    account: '',
+    email: '',
+    image: '',
+    ratings: 0.0,
 
-  constructor(private auth1: AuthusersService,private router:Router,private formbuilder:FormBuilder) { }
+  };
 
-  ngOnInit(): void {
-  }
+  constructor(
+    private auth1: AuthusersService,
+    private router: Router,
+    private formbuilder: FormBuilder,
+    private jwt: JwtService
+  ) {}
+
+  ngOnInit(): void {}
+
   loginForm1 = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]) ,
+    email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.min(6)]),
-  })
+  });
 
-  get f(): { [key: string]: AbstractControl } {
-    return this.loginForm1.controls;
+  onlogin(form: FormGroup) {
+    //Sign in the User to the to the app
+    this.auth1.loginData(form.value).subscribe(
+      (results: any) => {
+        this.auth1.saveToken(results.token);
+        this.user = this.jwt.getData(results.token);
+
+        if(this.user!=null) {
+          sessionStorage.setItem('role', this.user.account);
+          this.role = this.user.account;
+       }
+
+
+        if (this.role == 'PARENT') {
+          this.router.navigateByUrl('/parent-home');
+        } else if (this.role == 'OWNER') {
+          this.router.navigateByUrl('/owner-home');
+        } else if (this.role == 'ADMIN') {
+          this.router.navigateByUrl('/admin/schools');
+        }
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
   }
-  get formValidation(): { [key: string]: AbstractControl } {
-    return this.loginForm1.controls;
-  }
-
-
-  onlogin(form:FormGroup){
-
-    console.log(form.value)
- //Add the User to the Database
- this.auth1.loginData(form.value).subscribe((results)=>{
-
-  if(results === 'Enter correct password!' || results === ''){
-
-    alert('invalid login details')
-
-    console.log('invalid')
-
-    return
-   }
-   else{
-
-    this.router.navigateByUrl('/home')
-    console.warn('sucess');
-    alert('successfully logged in');
-
-console.log(results)
-    //routing to home page after successfully loging in
-
-
-
-
-
-   }
- })
-}
-
-
 }
--- Frontend/src/app/components/register/register.component.ts
+++ Frontend/src/app/components/register/register.component.ts
@@ -3,63 +3,78 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthusersService } from 'src/app/services/authusers.service';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { JwtService } from 'src/app/services/jwt.service';
+import { User } from 'src/app/interfaces/user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  role: any;
+  user: User | null = {
+    user_id: 0,
+    name: '',
+    surname: '',
+    account: '',
+    email: '',
+    image: '',
+    ratings: 0.0,
+  };
 
-  constructor(private auth: AuthusersService, private router : Router) { }
+  constructor(
+    private auth: AuthusersService,
+    private router: Router,
+    private jwt: JwtService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   registerForm1 = new FormGroup({
-
-    email: new FormControl('',[Validators.required, Validators.email]),
-    name: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]$')]),
-    surname: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z ]$')]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]$'),
+    ]),
+    surname: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z ]$'),
+    ]),
     password: new FormControl('', [Validators.required, Validators.min(6)]),
     account: new FormControl(),
     confirmPassword: new FormControl(),
-    gender :new FormControl(),
-  })
-  step=1
+    gender: new FormControl(),
+  });
+  step = 1;
 
-  nextStep(form:FormGroup)
-    {
-      this.step = 2;
-
-    }
-
-    onRegister(form:FormGroup){
-      if (this.registerForm1.valid) {
-        // submit the form
-        }
-      console.log(form.value)
-   //Add the User to the Database
-   this.auth.postData(form.value).subscribe((results)=>{
-     if(results == 'User added to database'){
-
-      console.warn('sucess');
-      alert('successfully registered');
-
-      this.router.navigateByUrl('/home')
-
-
-     }
-     else{
-      alert(results)
-
-     }
-
-   })
+  nextStep(form: FormGroup) {
+    this.step = 2;
   }
 
+  onRegister(form: FormGroup) {
+    this.auth.postData(form.value).subscribe(
+      (results: any) => {
+        this.auth.saveToken(results.token);
+        this.user = this.jwt.getData(results.token);
 
+        if (this.user != null) {
+          sessionStorage.setItem('role', this.user.account);
+          this.role = this.user.account;
+        }
 
+        if (this.role == 'PARENT') {
+          this.router.navigateByUrl('/parent-home');
+        } else if (this.role == 'OWNER') {
+          this.router.navigateByUrl('/owner-home');
+        } else if (this.role == 'ADMIN') {
+          this.router.navigateByUrl('/admin/schools');
+        }
+      },
 
-
-
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 }
